bd81fc0609862bf093731be0583a43f4102f84d4
Andy Wright <atwright147@gmail.com>
2026-04-30 17:16:05 +0100

    add @tanstack/svelte-query

--- fe-svelte/src/routes/+layout.svelte
+++ fe-svelte/src/routes/+layout.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
+
+	const queryClient = new QueryClient()
+
 	import { page } from '$app/state';
 	import { AppBar, AppLayout, NavItem } from 'svelte-ux';
 
@@ -12,24 +17,34 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<AppLayout>
-	<svelte:fragment slot="nav">
-		<NavItem
-			text="Home"
-			currentUrl={page.url}
-			path="/"
-			classes={{ root: "pl-3", active: "bg-primary/10 text-primary" }}
-		/>
-		<NavItem
-			text="About"
-			currentUrl={page.url}
-			path="/about"
-			classes={{ root: "pl-3", active: "bg-primary/10 text-primary" }}
-		/>
-	</svelte:fragment>
+<QueryClientProvider client={queryClient}>
+	<AppLayout>
+		<svelte:fragment slot="nav">
+			<NavItem
+				text="Home"
+				currentUrl={page.url}
+				path="/"
+				classes={{ root: "pl-3", active: "bg-primary/10 text-primary" }}
+			/>
+			<NavItem
+				text="About"
+				currentUrl={page.url}
+				path="/about"
+				classes={{ root: "pl-3", active: "bg-primary/10 text-primary" }}
+			/>
+			<NavItem
+				text="Contacts"
+				currentUrl={page.url}
+				path="/contacts"
+				classes={{ root: "pl-3", active: "bg-primary/10 text-primary" }}
+			/>
+		</svelte:fragment>
 
-	<AppBar title="Example" />
-	<main>
-		{@render children()}
-	</main>
-</AppLayout>
+		<AppBar title="Example" />
+		<main>
+			{@render children()}
+		</main>
+	</AppLayout>
+
+  <SvelteQueryDevtools />
+</QueryClientProvider>
---
+++ fe-svelte/src/routes/contacts/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { createQuery } from '@tanstack/svelte-query';
+
+	const query = createQuery(() => ({
+    queryKey: ['todos'],
+    queryFn: () => fetch('https://jsonplaceholder.typicode.com/todos').then(res => res.json()),
+  }))
+</script>
+
+<h1>Contacts</h1>
+
+<pre>{JSON.stringify(query.data, null, 2)}</pre>
