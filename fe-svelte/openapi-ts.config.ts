fe9ecd529bdaee25974377d187ef655ea542cfe2
Andy Wright <atwright147@gmail.com>
2026-05-04 22:48:18 +0100

    initial pass at adding Hey API

---
+++ fe-svelte/openapi-ts.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from '@hey-api/openapi-ts';
+
+export default defineConfig({
+	input: 'http://localhost:5077/swagger/v1/swagger.json',
+	output: 'src/client',
+	plugins: ['@tanstack/react-query']
+});
