e402d168da5e4711aa34445f29863b5132c23b15
Andy Wright <atwright147@gmail.com>
2026-05-05 21:53:02 +0100

    oops, should be using svelte-query

--- fe-svelte/src/routes/contacts/+page.svelte
+++ fe-svelte/src/routes/contacts/+page.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
 	import { createQuery } from '@tanstack/svelte-query';
+  import { getApiContacts } from '$lib/../client/sdk.gen';
+  import type { Contact } from '$lib/../client/types.gen';
 
-	const query = createQuery(() => ({
+  const query = createQuery<Contact[]>(() => ({
     queryKey: ['contacts'],
-    queryFn: () => fetch('https://localhost:7053/api/Contacts').then(res => res.json()),
+    queryFn: async () => {
+      const { data } = await getApiContacts({ throwOnError: true });
+      return data;
+    }
   }));
 </script>
 
 <h1>Contacts</h1>
+
+{#if query.isPending}
+  Loading...
+{:else if query.isError}
+  Error: {query.error.message}
+{:else}
+  {#each query.data ?? [] as contact (contact.id ?? `${contact.firstName}-${contact.lastName}-${contact.email}`)}
+    <p>{contact.firstName} {contact.lastName} ({contact.email})</p>
+  {/each}
+{/if}
 
 <details>
 	<summary>Raw Data</summary>
