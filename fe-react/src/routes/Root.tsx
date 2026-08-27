13184a31923598442bd2ea15950470c597b3e005
Andy Wright <atwright147@gmail.com>
2026-08-27 21:33:57 +0100

    add Icons, NavLinkList component and integrate with Root route

--- fe-react/src/routes/Root.tsx
+++ fe-react/src/routes/Root.tsx
@@ -1,40 +1,55 @@
-import { AppShell, Burger, NavLink } from '@mantine/core';
+import { AppShell, Burger } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { Link as RavigerLink } from 'raviger';
 import type { JSX, ReactNode } from 'react';
 
-// const links = [
-//   {
-//     text: 'Home',
-//     icon: <HomeIcon />,
-//     path: '/',
-//   },
-//   {
-//     text: 'Messages',
-//     icon: <ForumIcon />,
-//     path: '/messages',
-//   },
-//   {
-//     text: 'Calls',
-//     icon: <PhoneIcon />,
-//     path: '/calls',
-//   },
-//   {
-//     text: 'Contacts',
-//     icon: <GroupIcon />,
-//     path: '/contacts',
-//   },
-//   {
-//     text: 'Calendar',
-//     icon: <EventNoteIcon />,
-//     path: '/calendar',
-//   },
-//   {
-//     text: 'Settings',
-//     icon: <SettingsIcon />,
-//     path: '/settings',
-//   },
-// ];
+import IconCalendar from '~icons/lucide/calendar';
+import IconHome from '~icons/lucide/home';
+import IconLogIn from '~icons/lucide/log-in';
+import IconMessageCircle from '~icons/lucide/message-circle';
+import IconPhone from '~icons/lucide/phone';
+import IconSettings from '~icons/lucide/settings';
+import IconUsers from '~icons/lucide/users';
+
+import { NavLinkList } from '../components/NavLinkList';
+
+
+const links = [
+  {
+    text: 'Home',
+    icon: <IconHome />,
+    path: '/',
+  },
+  {
+    text: 'Messages',
+    icon: <IconMessageCircle />,
+    path: '/messages',
+  },
+  {
+    text: 'Calls',
+    icon: <IconPhone />,
+    path: '/calls',
+  },
+  {
+    text: 'Contacts',
+    icon: <IconUsers />,
+    path: '/contacts',
+  },
+  {
+    text: 'Calendar',
+    icon: <IconCalendar />,
+    path: '/calendar',
+  },
+  {
+    text: 'Settings',
+    icon: <IconSettings />,
+    path: '/settings',
+  },
+  {
+    text: 'Login',
+    icon: <IconLogIn />,
+    path: '/login',
+  },
+];
 
 export const Root = ({ children }: { children: ReactNode }): JSX.Element => {
   const [opened, { toggle }] = useDisclosure();
@@ -56,12 +71,11 @@ export const Root = ({ children }: { children: ReactNode }): JSX.Element => {
           hiddenFrom="sm"
           size="sm"
         />
+        Header
       </AppShell.Header>
 
       <AppShell.Navbar>
-        <NavLink component={RavigerLink} label="Home" href="/" />
-        <NavLink component={RavigerLink} label="Login" href="/login" />
-        <NavLink component={RavigerLink} label="Contacts" href="/contacts" />
+        <NavLinkList links={links} />
       </AppShell.Navbar>
 
       <AppShell.Main>{children}</AppShell.Main>
