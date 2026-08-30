98dad5b2cee0dcb0da045d4d62a1b5d5b598b2f0
Andy Wright <atwright147@gmail.com>
2026-08-30 16:02:06 +0100

    refactor Nav and show login/logout link dynamically

---
+++ fe-react/src/components/Nav.tsx
@@ -0,0 +1,116 @@
+import { Group, NavLink } from '@mantine/core';
+import { Link as RavigerLink } from 'raviger';
+import type { JSX, ReactNode } from 'react';
+
+import { useAuthStore } from '~stores/authStore';
+
+import IconCalendar from '~icons/lucide/calendar';
+import IconHome from '~icons/lucide/home';
+import IconLogIn from '~icons/lucide/log-in';
+import IconLogOut from '~icons/lucide/log-out';
+import IconMessageCircle from '~icons/lucide/message-circle';
+import IconPhone from '~icons/lucide/phone';
+import IconSettings from '~icons/lucide/settings';
+import IconUsers from '~icons/lucide/users';
+
+interface Link {
+  text: string;
+  icon: ReactNode;
+  path: string;
+}
+
+export interface Props {
+  links: Link[];
+}
+
+export function Nav(): JSX.Element {
+  const isAuthenticated = useAuthStore((state) => state.isAuthorised);
+
+  return (
+    <>
+      <NavLink
+        component={RavigerLink}
+        label={
+          <Group>
+            <IconHome /> Home
+          </Group>
+        }
+        href="/"
+      />
+
+      <NavLink
+        component={RavigerLink}
+        label={
+          <Group>
+            <IconMessageCircle /> Messages
+          </Group>
+        }
+        href="/messages"
+      />
+
+      <NavLink
+        component={RavigerLink}
+        label={
+          <Group>
+            <IconPhone /> Calls
+          </Group>
+        }
+        href="/calls"
+      />
+
+      <NavLink
+        component={RavigerLink}
+        label={
+          <Group>
+            <IconUsers /> Contacts
+          </Group>
+        }
+        href="/contacts"
+      />
+
+      <NavLink
+        component={RavigerLink}
+        label={
+          <Group>
+            <IconCalendar /> Calendar
+          </Group>
+        }
+        href="/calendar"
+      />
+
+      <NavLink
+        component={RavigerLink}
+        label={
+          <Group>
+            <IconSettings /> Settings
+          </Group>
+        }
+        href="/settings"
+      />
+
+      {isAuthenticated && (
+        <NavLink
+          component={RavigerLink}
+          label={
+            <Group>
+              <IconLogOut /> Logout
+            </Group>
+          }
+          href="/logout"
+        />
+      )}
+
+      {!isAuthenticated && (
+        <NavLink
+          component={RavigerLink}
+          label={
+            <Group>
+              <IconLogIn /> Login
+            </Group>
+          }
+          href="/login"
+        />
+      )}
+    </>
+  );
+}
--- fe-react/src/routes/Root.tsx
+++ fe-react/src/routes/Root.tsx
@@ -2,53 +2,7 @@ import { AppShell, Burger } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import type { JSX, ReactNode } from 'react';
 
-import IconCalendar from '~icons/lucide/calendar';
-import IconHome from '~icons/lucide/home';
-import IconLogIn from '~icons/lucide/log-in';
-import IconMessageCircle from '~icons/lucide/message-circle';
-import IconPhone from '~icons/lucide/phone';
-import IconSettings from '~icons/lucide/settings';
-import IconUsers from '~icons/lucide/users';
-
-import { NavLinkList } from '../components/NavLinkList';
-
-const links = [
-  {
-    text: 'Home',
-    icon: <IconHome />,
-    path: '/',
-  },
-  {
-    text: 'Messages',
-    icon: <IconMessageCircle />,
-    path: '/messages',
-  },
-  {
-    text: 'Calls',
-    icon: <IconPhone />,
-    path: '/calls',
-  },
-  {
-    text: 'Contacts',
-    icon: <IconUsers />,
-    path: '/contacts',
-  },
-  {
-    text: 'Calendar',
-    icon: <IconCalendar />,
-    path: '/calendar',
-  },
-  {
-    text: 'Settings',
-    icon: <IconSettings />,
-    path: '/settings',
-  },
-  {
-    text: 'Login',
-    icon: <IconLogIn />,
-    path: '/login',
-  },
-];
+import { Nav } from '../components/Nav';
 
 export const Root = ({ children }: { children: ReactNode }): JSX.Element => {
   const [opened, { toggle }] = useDisclosure();
@@ -69,7 +23,7 @@ export const Root = ({ children }: { children: ReactNode }): JSX.Element => {
       </AppShell.Header>
 
       <AppShell.Navbar>
-        <NavLinkList links={links} />
+        <Nav />
       </AppShell.Navbar>
 
       <AppShell.Main>{children}</AppShell.Main>
